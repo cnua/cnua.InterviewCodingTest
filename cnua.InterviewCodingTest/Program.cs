f9ee99618f1cddf68c9c78015c2a24641bdb1c15
azprivate web master <cnua@hotmail.com>
2023-08-25 20:38:07 +0100

    Add project files.

---
+++ cnua.InterviewCodingTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cnua.InterviewCodingTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
